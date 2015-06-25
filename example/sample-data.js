9806d6b6c170dc1e01577f3c52d847a117c15d01
Branden Horiuchi <bhoriuchi@gmail.com>
2015-06-25 13:30:15 -0700

    fixing jslint warnings

--- example/sample-data.js
+++ example/sample-data.js
@@ -1,29 +1,24 @@
 module.exports = {
-		credential: [
-            {
-            	id: 1,
-            	name: 'cred1',
-            	username: 'cred1username',
-            	encryptedKey: '12345'
-            },
-            {
-            	id: 2,
-            	name: 'cred2',
-            	encryptedKey: '678910'
-            },
-            {
-            	id: 3,
-            	name: 'cred3',
-            	username: 'cred3username',
-            	encryptedKey: '678910',
-            	description: 'describe this'
-            },
-            {
-            	id: 4,
-            	name: 'cred4',
-            	username: 'cred4username',
-            	encryptedKey: '678910',
-            	val: 'dont add'
-            },
-		]
+	credential : [ {
+		id : 1,
+		name : 'cred1',
+		username : 'cred1username',
+		encryptedKey : '12345'
+	}, {
+		id : 2,
+		name : 'cred2',
+		encryptedKey : '678910'
+	}, {
+		id : 3,
+		name : 'cred3',
+		username : 'cred3username',
+		encryptedKey : '678910',
+		description : 'describe this'
+	}, {
+		id : 4,
+		name : 'cred4',
+		username : 'cred4username',
+		encryptedKey : '678910',
+		val : 'dont add'
+	}, ]
 };
