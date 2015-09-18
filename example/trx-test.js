4f53e10a98f24af9b1745f402d75d2d2dc594f37
Branden Horiuchi <bhoriuchi@gmail.com>
2015-09-18 02:00:04 -0700

    added transaction support between calls

---
+++ example/trx-test.js
@@ -0,0 +1,48 @@
+// Author: Branden Horiuchi <bhoriuchi@gmail.com>
+// Description: Example of usage
+//
+
+
+// create a database connection
+var db = {
+	"client": "mysql",
+	"connection": {
+		"host": "127.0.0.1",
+		"user": "db",
+		"password": "password",
+		"database": "test",
+		"charset": "utf8"
+	},
+	"debug": false
+};
+
+
+
+
+
+// import the modules
+var knex    = require('knex')(db);
+var schemer = require('../lib/schemer')(knex);
+var schema  = require('./schema')(schemer.constants);
+var data    = require('./sample-data');
+
+knex.transaction(function(trx) {
+	// drop the tables
+	schemer.drop(schema.v1, trx).then(function() {
+		// test a sync
+		return schemer.sync(schema.v1, trx).then(function(result) {
+			
+			// load the data
+			return schemer.convertAndLoad(data, schema.v1, trx);
+		});
+	});
+})
+.then(function() {
+	process.exit();
+});
+
+
+
+
+
+
