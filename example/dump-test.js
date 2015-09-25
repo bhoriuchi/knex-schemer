a78b6af6f1aa4e0121e02b0d4e92c870bb730dab
Branden Horiuchi <bhoriuchi@gmail.com>
2015-09-25 13:22:32 -0700

    added dump method

---
+++ example/dump-test.js
@@ -0,0 +1,48 @@
+// Author: Branden Horiuchi <bhoriuchi@gmail.com>
+// Description: Example of usage
+//
+
+
+// create a database connection
+var db = {
+	"client": "mysql",
+	"connection": {
+		"host": "127.0.0.1",
+		"user": "db",
+		"password": "password",
+		"database": "test",
+		"charset": "utf8"
+	},
+	"debug": false
+};
+
+
+
+
+
+// import the modules
+var knex    = require('knex')(db);
+var schemer = require('../lib/schemer')(knex);
+var schema  = require('./schema')(schemer.constants);
+var data    = require('./sample-data');
+
+//drop the tables
+schemer.drop(schema.v1).then(function() {
+	// test a sync
+	return schemer.sync(schema.v1).then(function(result) {
+		
+		// load the data
+		return schemer.convertAndLoad(data, schema.v1).then(function() {
+			
+			// dump the data
+			return schemer.dump(schema.v1).then(function(data) {
+				console.log(data);
+				process.exit();
+			});
+		});
+	});
+});
+
+
+
+
